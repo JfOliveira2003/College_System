4e4d2b9bf2a2d27b853ce30b227c9516c1c2fcec
JfOliveira2003 <fabriciojosemacedo@gmail.com>
2024-11-12 22:48:19 -0300

    deleting server.ts and making some improvements in the routes dir

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,47 @@
 import express from 'express';
 
-const route = require('./routes')
+const auth = require('./routes/authenticationRoute');
+const teacher = require('./routes/teacherRoute');
+const student = require('./routes/studentRoute');
+const course = require('./routes/courseRoute');
+
 const app = express();
 const port : number = 3000; 
 
 
-app.use(route.about);
+app.use(student.createStudent);
+app.use(student.deleteStudent);
+app.use(student.getStudent);
+app.use(student.updateStudent);
+app.use(student.getAllStudent);
 
-app.use(route.Login);
 
-app.use(route.auth)
-app.use(route.test);
+app.use(teacher.createTeacher);
+app.use(teacher.deleteTeacher)
+app.use(teacher.getTeacher);
+app.use(teacher.updateTeacher);
+app.use(teacher.getAllTeacher);
+
+app.use(course.createCourse);
+app.use(course.deleteCourse);
+app.use(course.getCourse);
+app.use(course.updateCourse);
+
+
+app.use(course.createSubject)
+app.use(course.updateSubject)
+app.use(course.deleteSubject)
+app.use(course.getSubject)
+app.use(course.getAllSubjects);
+
+
+app.use(auth.Login)
+app.use(auth.Register)
+app.use(auth.Logout)
+app.use(auth.forgetPassword)
+
+
+app.use(auth.resetPassword);
+
+
 app.listen(port);
